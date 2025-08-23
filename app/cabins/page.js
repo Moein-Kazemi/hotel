d04d3804f79d47f74e714973ceabf3af4d527747
Moein-Kazemi <moeinkazemi.dev@gmail.com>
2025-08-23 14:15:04 +0330

    organize structure of the Project

--- app/cabins/page.js
+++ app/cabins/page.js
@@ -1,17 +1,9 @@
-import Counter from "../components/Counter";
+import Counter from "@/app/_components/Counter";
 
-export default async function page() {
-  const res = await fetch("https://jsonplaceholder.typicode.com/users");
-  const users = await res.json();
+export default function page() {
   return (
     <div>
       <h1>Cabins page</h1>
-      <ul>
-        {users.map((user) => (
-          <li key={user.id}>{user.name}</li>
-        ))}
-      </ul>
-      <Counter users={users} />
     </div>
   );
 }
