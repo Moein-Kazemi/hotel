022bc5ad1e3083b08975d63078c172a021ee1de1
Moein-Kazemi <moeinkazemi.dev@gmail.com>
2025-08-27 13:52:48 +0330

    create layout for Account and complite Account page

--- app/account/page.js
+++ app/account/page.js
@@ -2,5 +2,9 @@ export const metadata = {
   title: "Guests Account",
 };
 export default function page() {
-  return <h1>Account</h1>;
+  return (
+    <h1 className="font-semibold text-2xl text-accent-400 mb-7 ">
+      Welcome / Moein
+    </h1>
+  );
 }
