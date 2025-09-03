54c363ad81eadee1d79d4bfef7ab2383ac35d0c9
Moein-Kazemi <moeinkazemi.dev@gmail.com>
2025-09-03 14:34:22 +0330

    convert [cabinId] route from Dynamic to static

--- app/cabins/[cabinId]/page.js
+++ app/cabins/[cabinId]/page.js
@@ -1,14 +1,22 @@
-import { getCabin } from "@/app/_lib/data-service";
+import { getCabin, getCabins } from "@/app/_lib/data-service";
 import { EyeSlashIcon, MapPinIcon, UsersIcon } from "@heroicons/react/24/solid";
 import Image from "next/image";
 
 export async function generateMetadata({ params }) {
-  const id = params.cabinId;
+  const { name } = await getCabin(params.cabinId);
 
-  const path = `localhost:3000/cabins/${id}`;
   return {
-    title: path,
+    title: `Cabin ${name}`,
   };
+}
+
+export async function generateStaticParams() {
+  const cabins = await getCabins();
+
+  const ids = cabins.map((cabin) => {
+    cabinId: cabin.id;
+  });
+  return ids;
 }
 
 export default async function Page({ params }) {
--- app/loading.js
+++ app/loading.js
@@ -1,5 +1,5 @@
 import Spinner from "@/app/_components/Spinner";
 
 export default function Loading() {
-  return <div>loading data...</div>;
+  return <Spinner />;
 }
