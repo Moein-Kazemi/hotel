ea23973276d1bae166e09120c3a55f720a7e7557
Moein-Kazemi <moeinkazemi.dev@gmail.com>
2025-10-24 15:59:47 +0330

    using ISR strategi into CabinsList component

--- app/cabins/page.js
+++ app/cabins/page.js
@@ -2,6 +2,9 @@ import { Suspense } from "react";
 import CabinList from "@/app/_components/CabinList";
 import Spinner from "@/app/_components/Spinner";
 
+// the ISR component , because some informations of the cabin maybe cahnge after one houre
+// export const revalidate = 3600;
+
 export const metadata = {
   title: "Cabins",
 };
