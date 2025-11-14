03ccaad5966a502d680c010fde8b4d4d152f348b
Moein-Kazemi <moeinkazemi.dev@gmail.com>
2025-11-14 23:39:12 +0330

    add reservationContext and add API end points to see the cabin info in the json format

---
+++ app/_components/ReservationContext.js
@@ -0,0 +1,27 @@
+"use client";
+import { createContext, useContext, useState } from "react";
+
+const ReservationContext = createContext();
+const defaultReservationState = { from: undefined, to: undefined };
+
+function ReservationProvider({ children }) {
+  const [range, setRange] = useState(defaultReservationState);
+  const resetRange = () => setRange(defaultReservationState);
+
+  return (
+    <ReservationContext.Provider value={{ range, setRange, resetRange }}>
+      {children}
+    </ReservationContext.Provider>
+  );
+}
+
+function useReservation() {
+  const context = useContext(ReservationContext);
+  console.log(context);
+
+  if (context === undefined)
+    throw new Error("The context was using outside of the Provider");
+  return context;
+}
+
+export { ReservationProvider, useReservation };
