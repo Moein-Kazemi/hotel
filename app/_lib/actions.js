e4a1223d5a7b57a3b6b9826ad6012d43ed16e625
Moein-Kazemi <moeinkazemi.dev@gmail.com>
2026-02-07 14:08:37 +0330

    add updateProfileAction to the action.js for update user profile

--- app/_lib/actions.js
+++ app/_lib/actions.js
@@ -1,6 +1,7 @@
 "use server";
 
-import { signIn, signOut } from "./auth";
+import { auth, signIn, signOut } from "./auth";
+import { supabase } from "./supabase";
 
 export async function signInAction() {
   // when the signIn becomes true go ro redirectTo:
@@ -9,4 +10,29 @@ export async function signInAction() {
 
 export async function signOutAction() {
   await signOut({ redirectTo: "/" });
+}
+
+export async function updateProfileAction(formData) {
+  const session = await auth();
+  if (!session) throw new Error("You must to logged in first !");
+
+  const nationalID = formData.get("nationalID");
+  const [nationality, countryFlag] = formData.get("nationality").split("%");
+
+  if (!/^[A-Za-z0-9]{6,12}$/.test(nationalID))
+    throw new Error("please provide a valid nationalID");
+
+  const updateData = { nationality, nationalID, countryFlag };
+
+  const { data, error } = await supabase
+    .from("guests")
+    .update(updateData)
+    .eq("id", session.user.guestId)
+    .select()
+    .single();
+
+  if (error) {
+    console.error(error);
+    throw new Error("Guest could not be updated");
+  }
 }
